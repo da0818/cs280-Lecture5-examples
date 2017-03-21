972e8a4ea269fe6e0d6076f4cf8ad3b80dbf1766
da0818 <chuntawu@gmail.com>
2017-03-21 14:25:37 +0800

    新增Private語法Get方法Set方法

---
+++ DALibrary1/product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALibrary1
+{
+    public class Product
+    {
+        public string Name;
+        private int price;
+        public float Discount;
+
+        public int GetPrice()
+        {
+            return this.price;
+        }
+
+        public void SetPrice(int price)
+        {
+            if (price > 200)
+            {
+                this.price = 200;
+            }
+            else if (price < 39)
+            {
+                this.price = 39;
+            }
+            else
+            {
+                this.price = price;
+            }
+        }
+    }
+}
---
+++ example01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALibrary1;
+
+namespace example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Console.Write("請輸入產品名稱");
+            product.Name = Console.ReadLine();
+
+            Console.Write("請輸入{0}的價錢:", product.Name);
+            try
+            {
+                int price = 0;
+                price = int.Parse(Console.ReadLine());
+                product.SetPrice(price);
+                Console.WriteLine("產品:{0}的價格是{1}", product.Name, product.GetPrice());
+            }
+            catch
+            {
+                Console.WriteLine("價格輸入錯誤");
+                return;
+            }
+
+            Product product2 = new Product();
+            Console.Write("請輸入產品名稱")
+
+            int price2 = 0;
+            price2 = int.Parse(Console.ReadLine());
+            product2.SetPrice(price2);
+        }
+    }
+}
